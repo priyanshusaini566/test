2664e7059ce3dcc95e13822de196e4bc1f62c021
priyanshu saini <ps56646i4@gmail.com>
2026-07-22 23:39:41 +0530

    new commit

---
+++ problem7.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    
+    if(a>=b){
+        if(a>=c){
+            if(a>=d){
+                cout<<a;
+            }
+            else{
+                cout<<d;
+            }
+        }
+        else if(c>=d){
+            cout<<c;
+        }
+        else{
+            cout<<d;
+        }
+    }
+    else if(b>=c){
+        if (b>=d){
+            cout<<b;
+        }
+        else{
+            cout<<d;
+        }
+    }
+    else{
+         if(c>=d){
+            cout<<c;
+    }
+         else{
+            cout<<d;
+    }
+}
+}
+
