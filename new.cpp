f80806cf0f26bf98cbb41a42769b107958104d18
priyanshu saini <ps56646i4@gmail.com>
2026-07-25 22:57:52 +0530

    new commit

---
+++ new.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch='A';
+    cout<<"the character is : "<<ch;
+    
+    cout<<"the ascii value is : "<<int(ch);
+}
---
+++ ps.java
@@ -0,0 +1,8 @@
+public class ps {
+    public static void main(String args[]){
+
+        char ch='A';
+        System.out.println("the ascii value is : "+(int)ch);
+        
+    }
+}
