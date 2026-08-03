e39be672a34c1929ab6a635b30889defaf3b2b2e
priyanshu saini <ps56646i4@gmail.com>
2026-08-03 22:11:25 +0530

    new commit

---
+++ test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class test{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to print : ");
+        int a=sc.nextInt();
+        System.out.println(a);
+    }
+}
