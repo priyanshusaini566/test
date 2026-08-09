d61172173b291c357bedad076cdf3a90fbdaf434
priyanshu saini <ps56646i4@gmail.com>
2026-08-09 22:50:52 +0530

    new commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class hello{
+    public static void main(String[] args){
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a : ");
+        int a=sc.nextInt();
+        System.out.println("the value of int will be : " +a);
+        
+    }
+}
