41311adc69721dd9ef65fa2c35f051f06189947e
priyanshu saini <ps56646i4@gmail.com>
2026-08-14 23:08:56 +0530

    new commit

--- armstrong.java
+++ armstrong.java
@@ -1,8 +1,24 @@
 import java.util.*;
 public class armstrong{
     public static void main(String[] args){
-        int a=10;
-        System.out.println("the value will be : " +a);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num=sc.nextInt();
+        int n=num;
+        int count=0;
+
+        while(num>0){
+            int rem=num%10;
+            count=count+(rem*rem*rem);
+            num=num/10;
+
+        }
+        if (count==n) {
+            System.out.println("The number is an armstrong");
+        }
+        else{
+            System.out.println("number is not armstrong");
+        }
 
     }
 }
