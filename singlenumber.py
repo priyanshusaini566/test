43571b7fc1dba8ad5f8eb09991c049e6fcd87f5b
priyanshu saini <ps56646i4@gmail.com>
2026-09-03 22:10:38 +0530

    ps is back

---
+++ singlenumber.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums.count(nums[i])==1:
+                return nums[i]
+        
+
+obj=Solution()
+print(obj.singleNumber([1,2,1,2,4]))
